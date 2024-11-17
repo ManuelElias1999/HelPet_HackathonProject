c7ec2673dac6508e0cf2697d81e04c1e0e8a2682
ManuelElias <elias.soria.juan.manuel@gmail.com>
2024-11-16 21:47:33 -0400

    registerUsers tested

--- test/registerUsers.t.sol
+++ test/registerUsers.t.sol
@@ -6,21 +6,48 @@ import {RegisterUsers} from "../src/registerUsers.sol";
 
 /**
  * To run this contract, copy and paste this command in the terminal:
- * forge test -vvvv --match-path test/registerUsers.t.sol --fork-url https://rpc.test.taiko.xyz
+ * forge test -vvvv --match-path test/registerUsers.t.sol --fork-url https://hekla.taikoscan.io
  * 
  * @dev Contract deployed on Taiko Hekla
- * https://explorer.test.taiko.xyz/address/0xd28eb2D29964127D102cD0047A1fee319B328Bca
+ * https://hekla.taikoscan.io/address/0xd28eb2D29964127D102cD0047A1fee319B328Bca#code
 */
 
 contract RegisterUsersTest is Test {
     RegisterUsers public registerUsers;
-    address owner = 0x5B753Da5d8c874E9313Be91fbd822979Cc7F3F88; // Owner and Agent of the real contract
-    address agent = 0x5B753Da5d8c874E9313Be91fbd822979Cc7F3F88; // Agent of the real contract
-    address user = makeAddr("user");
-    address entity = makeAddr("entity");
+    address owner = 0xd806A01E295386ef7a7Cea0B9DA037B242622743; // Owner and agent of the real contract
+    address user1 = 0xFc6623B340A505E6819349aF6beE2333D31840E1; // User 1
+    address user2 = 0x9F693ea18DA08824E729d5efc343Dd78254a9302; // User 2
 
     function setUp() public {
         registerUsers = RegisterUsers(0xd28eb2D29964127D102cD0047A1fee319B328Bca);
+    }
+
+    // ------------------------------------- Register User ----------------------------------- 
+    function test_registerUser() public {
+        vm.startPrank(owner);
+        registerUsers.registerUser(user1);
+        vm.stopPrank();
+    }
+
+    function test_fail_registerUser() public {
+        vm.startPrank(user1);
+        vm.expectRevert("Ownable: caller is not the owner");
+        registerUsers.registerUser(user1);
+        vm.stopPrank();
+    }
+
+    // ------------------------------------- Register Entity ----------------------------------- 
+    function test_registerEntity() public {
+        vm.startPrank(owner);
+        registerUsers.registerEntity(user2);
+        vm.stopPrank();
+    }
+
+    function test_fail_registerEntity() public {
+        vm.startPrank(user2);
+        vm.expectRevert("Ownable: caller is not the owner");
+        registerUsers.registerEntity(user2);
+        vm.stopPrank();
     }
 
 }
